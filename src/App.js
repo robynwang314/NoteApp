b0d7a382729e6351d1fa70eece2e6db9ab7a4bbb
Robyn Wang <tyrobynwang@gmail.com>
2020-01-08 23:51:44 +0000

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Nav from './compoents/Nav';
+import Nav from './components/Nav';
 
 class App extends Component {
 render() {
