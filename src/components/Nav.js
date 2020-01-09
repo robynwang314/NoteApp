ea221d423342ee8d1b57f1864ad50764788801b9
Robyn Wang <tyrobynwang@gmail.com>
2020-01-09 01:00:17 +0000

    lesson 7

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Nav extends React.Component {
+class Nav extends Component {
  render() {
+    const { toggleNote, showNote } = this.props;
+
    return (
      <div className="nav-container">
       <div className="nav-logo">Note</div>
-      <div className="nav-button">
-        + Note
+      <div className="nav-button" onClick={() => toggleNote()}>
+        { showNote? 'Cancel' : '+ Note' } 
         </div>
      </div>
    );
