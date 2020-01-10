b3c7e01a8facd427e1c479b3375a45720213977e
Robyn Wang <tyrobynwang@gmail.com>
2020-01-10 01:36:08 +0000

    lesson 15

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      showNote: false
+      showNote: false,
+      notes: []
     };
   }
 
@@ -20,18 +22,25 @@ class App extends Component {
   }
 
   getNotes = () => {
-    axios.get('https://firehose-note-api.herokuapp.com/notes')
-    .then((res) => console.log(res.data) )
+    axios.get(urlFor('notes'))
+    .then((res) => this.setState({ notes: res.data}) )
     .catch((err) => console.log(err.resonse.data) );
   } 
 
   render() {
-    const { showNote } = this.state;
+    const { showNote, notes } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote? <Note /> : <List getNotes={this.getNotes} /> }
+        { showNote? 
+          <Note /> 
+          : 
+          <List 
+            getNotes={this.getNotes}
+            notes={notes}
+          /> 
+        }
       </div>
     );
   }
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+  return  'https://note-api-robyn-wang.herokuapp.com/' + endpoint;
+};
+
+export default urlFor;
